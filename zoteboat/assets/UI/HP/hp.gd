d8f6a1bdc19dacd8f3e9433f45758d9b75355830
Thomas Terlouw <thomasterlouw888@gmail.com>
2025-11-28 11:59:36 +0100

    W.I.P hp

--- zoteboat/assets/UI/HP/hp.gd
+++ zoteboat/assets/UI/HP/hp.gd
@@ -5,12 +5,14 @@ var hearts: Array[Control] = []
 var space_between := 60
 
 var hp: int
+var max_hp: int
 
 func _ready() -> void:
 	await get_tree().process_frame
 	await get_tree().process_frame
 	
 	hp = Global.player.health
+	max_hp = Global.player.max_health
 	setup_ui()
 	Global.player.player_health_changed.connect(_on_hp_change)
 
@@ -24,6 +26,9 @@ func setup_ui():
 
 
 func _on_hp_change(current_hp):
+	
+	
+	
 	for i in range(hearts.size()):
 		hearts[i].set_full(i < current_hp)
 	hp = current_hp
