a0c47f08f17fb11f0bcbd35d6838c418035edcbd
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-01-27 12:54:25 +0100

    tiktik animations

--- zoteboat/enemies/normal/tiktik/tiktik.gd
+++ zoteboat/enemies/normal/tiktik/tiktik.gd
@@ -8,6 +8,23 @@ signal killed(node: Node2D)
 
 @export var speed: float = 100
 @onready var path_follow_2d: PathFollow2D = $".."
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+var walking: bool = true
+
+
+var current_anim: String
+
+enum ANIM_PRIORITY {
+	WALK,
+	STUN,
+	DEATH,
+	LAND_DEATH
+}
+
+var current_anim_priority: int = 0
+
+var dead: bool = false
 
 func _ready() -> void:
 	if !start_active:
@@ -19,6 +36,8 @@ func _ready() -> void:
 	
 	#stats.health_changed.connect(_on_health_changed)
 	stats.health_depleted.connect(_on_health_depleted)
+	
+	sprite_2d.animation_finished.connect(_on_animation_finished)
 
 func activate():
 	set_process(true)
@@ -34,6 +53,9 @@ func deactivate():
 
 
 func damage(damage_value: int):
+	play_anim("stun")
+	walking = false
+	
 	stats.health -= damage_value
 
 func i_frames(time):
@@ -42,10 +64,51 @@ func i_frames(time):
 	remove_from_group("invincible")
 
 func _on_health_depleted():
-	killed.emit(self)
-
-
-
+	play_anim("death(air)")
+	var dir = sign(global_position - Global.player.global_position)
+	
+	velocity = Vector2(300*dir.x, -800)
+	dead = true
+	
+	self.add_to_group("invincible")
 
 func _process(delta: float) -> void:
+	if dead:
+		velocity.y += 30
+		move_and_slide()
+		if is_on_floor():
+			play_anim("death(land)")
+		
+		return
+	
+	if !walking:
+		return
 	path_follow_2d.progress += speed * delta
+
+
+
+
+func play_anim(anim_name: String = "idle", priority: int = 0):
+	if priority < current_anim_priority:
+		return
+	
+	if anim_name == "death(land)":
+		velocity = Vector2.ZERO
+	
+	current_anim_priority = priority
+	
+	
+	current_anim = anim_name
+	
+	$Sprite2D.play(anim_name)
+
+func _on_animation_finished():
+	current_anim_priority = 0
+	
+	if current_anim == "stun":
+		await  get_tree().create_timer(0.2).timeout
+		play_anim("walk")
+		walking = true
+	
+	if current_anim == "death(land)":
+		killed.emit(self)
