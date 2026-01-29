22b690a48b2d16070b91da88e64ee4e0e9d10e63
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-01-29 20:48:39 +0100

    bench + changes

---
+++ zoteboat/maps/benches/bench.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var entered: bool = false
+
+@export var own_spawning_group: String = "bench"
+
+func _ready() -> void:
+	entered = false
+	$Sprite2D2.visible = false
+	$Sprite2D.play("default")
+	
+	add_to_group(own_spawning_group)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if !body.is_in_group("player"):
+		return
+	
+	entered = true
+	$Sprite2D2.visible = true
+	$Sprite2D.play("lit")
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if !body.is_in_group("player"):
+		return
+	
+	entered = false
+	$Sprite2D2.visible = false
+	$Sprite2D.play("default")
+
+
+func _physics_process(_delta: float) -> void:
+	if Global.player.direction.y == 1 && !Global.player.moving.active:
+		SaveLoad.contents_to_save.starting_room = Global.map.scene_file_path
+		SaveLoad.contents_to_save.starting_location = own_spawning_group
+		
+		SaveLoad._save()
