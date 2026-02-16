7899db87bcca279c00f963a0bf7902c3aace6ff0
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-02-16 15:52:18 +0100

    dissable movement changed to change_player_values

--- zoteboat/maps/map_placables/dissable movement/change_player_value.gd
+++ zoteboat/maps/map_placables/dissable movement/change_player_value.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@export var changed_stuff: Array[KeyValue] = []
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if !body.is_in_group("player") || Global.map_holder.is_transition:
 		return
 	
-	
-	
-	body.can_move = false
+	for kv in changed_stuff:
+		body.set(kv.key, kv.value)
