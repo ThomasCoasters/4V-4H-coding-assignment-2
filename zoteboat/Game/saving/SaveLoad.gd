4a51f797a8baaa06672e10028a9805a55b4ba888
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-02-21 20:04:24 +0100

    send help

--- zoteboat/Game/saving/SaveLoad.gd
+++ zoteboat/Game/saving/SaveLoad.gd
@@ -10,12 +10,12 @@ var DEFAULT_SAVE: Dictionary = {
 	"has_double_jump": false,
 	"has_wall_cling": false,
 	
-	#"starting_room": "res://maps/maps/pre-dash/kingspass_for_losers.tscn",
-	"starting_room": "res://maps/maps/Circus/Circus_1.tscn",
+	"starting_room": "res://maps/maps/pre-dash/kingspass_for_losers.tscn",
+	#"starting_room": "res://maps/maps/Circus/Circus_1.tscn",
 	#"starting_room": "res://maps/maps/P.o.B/ability_check.tscn",
 	
-	#"starting_location": "start_1,-1",
-	"starting_location": "bench",
+	"starting_location": "start_1,-1",
+	#"starting_location": "bench",
 	#"starting_location": "right_P.o.B",
 	
 	"rumble": 2,
