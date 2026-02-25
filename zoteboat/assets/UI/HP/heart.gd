6bb6d2cfc8c15f875cae147e969cbaef9b3b4c78
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-02-25 15:30:13 +0100

    W.I.P HP better

--- zoteboat/assets/UI/HP/heart.gd
+++ zoteboat/assets/UI/HP/heart.gd
@@ -1,5 +1,19 @@
 extends Control
+@onready var hearth: AnimatedSprite2D = $hearth
+var full: bool
+
 
 func set_full(value: bool):
-	$Full.visible = value
-	$Empty.visible = not value
+	if value:
+		full = true
+		hearth.play("full")
+	else:
+		hearth.play("empty")
+
+
+
+
+func _on_hearth_animation_finished() -> void:
+	if full:
+		full = false
+		hearth.play("shine")
