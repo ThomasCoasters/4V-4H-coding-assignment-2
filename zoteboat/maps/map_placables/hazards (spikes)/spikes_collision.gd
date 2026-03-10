3c5454973172b33dfe5dbcbc1e04317d1e3ef8b1
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-03-10 20:58:41 +0100

    OREN ZIJN OVERRATED

--- zoteboat/maps/map_placables/hazards (spikes)/spikes_collision.gd
+++ zoteboat/maps/map_placables/hazards (spikes)/spikes_collision.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @export var damage: int = 1
+@export var sawblade: bool = false
 
+func _ready() -> void:
+	if sawblade:
+		$Control.get_children().pick_random().playing = true
 
 func _on_static_body_2d_body_entered(body: Node2D) -> void:
 	if !body.is_in_group("player") || Global.map_holder.is_transition:
